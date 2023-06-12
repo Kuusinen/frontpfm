450804ed7a614d405009bfd4ae5a30d81c7282aa
Rogatien de La Brunetière <rogatien.delabrunetire@gmail.com>
2023-06-12 08:59:47 +0200

    prepare homepage to admin mode

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,16 +1,16 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
-import { Category } from '../model/category';
-import { CategoryService } from '../service/category.service';
+import { AuthService } from '../service/auth.service';
+import { carouselElement } from '../model/carouselElement';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
 
-  images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  private file!: File;
 
   paused = false;
   unpauseOnArrow = false;
@@ -19,6 +19,29 @@ export class HomepageComponent {
 
   @ViewChild('carousel', { static: true })
   carousel!: NgbCarousel;
+
+  isLog!: boolean;
+
+  carouselImages: carouselElement[] = [];
+
+  imgCarousel!: carouselElement;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.isLog = !this.authService.isTokenExpired();
+
+    let images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
+    let i = 1;
+    for (let img of images) {
+      this.imgCarousel = new carouselElement();
+      this.imgCarousel.byteImage = img;
+      this.imgCarousel.textDescription = "My slide " + i + " title";
+      this.carouselImages.push(this.imgCarousel);
+      i = i + 1;
+    }
+  }
 
   togglePaused() {
     if (this.paused) {
@@ -30,15 +53,29 @@ export class HomepageComponent {
   }
 
   onSlide(slideEvent: NgbSlideEvent) {
-    if (
-      this.unpauseOnArrow &&
-      slideEvent.paused &&
-      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
-    ) {
+    if (this.unpauseOnArrow && slideEvent.paused && (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
       this.togglePaused();
     }
     if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
       this.togglePaused();
     }
+  }
+
+  importImage(event: any): void {
+    const reader = new FileReader();
+    this.file = event.target.files[0];
+    reader.readAsDataURL(this.file);
+
+    reader.onload = () => {
+      const byteImage = reader.result as string;
+      this.imgCarousel = new carouselElement();
+      this.imgCarousel.byteImage = byteImage;
+      this.imgCarousel.textDescription = "";
+      this.carouselImages.push(this.imgCarousel);
+    }
+  }
+
+  deleteCarouselImg(img: carouselElement): void {
+    this.carouselImages.splice(this.carouselImages.indexOf(img), 1);
   }
 }
