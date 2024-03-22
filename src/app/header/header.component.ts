d5777ed471e10c396f43f3902c0181fffdc238b5
Rogatien de La Brunetière <rogatien.delabrunetire@gmail.com>
2024-03-22 15:26:58 +0100

    Add new font family and change text color for header and footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,25 +1,70 @@
+import { animate, state, style, transition, trigger, AnimationEvent } from '@angular/animations';
 import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger("change", [
+      state('stable', style({
+        opacity: 0,
+        transform: 'translateY(20px)'
+      })),
+      state('onChange', style({
+        opacity: 1,
+        transform: 'translateY(0px)'
+      })),
+      transition('stable => onChange', [
+        animate('.5s ease-in')
+      ])
+    ])]
 })
 export class HeaderComponent {
 
-  isCollapsed!: boolean;
+  isCollapsed!: boolean
+
+  state: string = "onChange"
+
+  imageSource: string = "assets/img/pfm_title_c.png"
 
   @ViewChild('title')
   private title!: ElementRef;
 
-  constructor(private renderer: Renderer2){
+  constructor(private renderer: Renderer2) {
     this.isCollapsed = true;
+
+    this.renderer.listen('window', 'scroll', (e) => {
+      if ((e.target.scrollingElement as Element).scrollTop > 300) {
+        this.changeImage(true)
+      } else {
+        this.changeImage(false)
+      }
+    });
   }
 
-  disableHamburgerMenu()
-  {
-    if( !this.isCollapsed){
+  changeImage(reduce: boolean) {
+    const oldSource = this.imageSource;
+    if (reduce) {
+      this.imageSource = "assets/img/pfm_title_r.png"
+    } else {
+      this.imageSource = "assets/img/pfm_title_c.png"
+    }
+
+    if(oldSource != this.imageSource){
+      this.state= "stable"
+    }
+  }
+
+  disableHamburgerMenu() {
+    if (!this.isCollapsed) {
       this.isCollapsed = true
+    }
+  }
+
+  onDoneEvent(event: AnimationEvent){
+    if(event.toState === 'stable'){
+      this.state= "onChange"
     }
   }
 }
